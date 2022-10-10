9118c8e14299cc46ef254c07038592503818edc5
Shiv <Shivad.Bhavsar@amd.com>
2022-10-10 20:14:57 +0000

    update branch for migraphx source install

--- tools/install_migraphx.sh
+++ tools/install_migraphx.sh
@@ -1,4 +1,4 @@
-MIGRAPHX_BRANCH=develop
+MIGRAPHX_BRANCH=master
 MIGRAPHX_REPO=https://github.com/ROCmSoftwarePlatform/AMDMIGraphX.git
 
 git clone --single-branch --branch $MIGRAPHX_BRANCH --recursive $MIGRAPHX_REPO
