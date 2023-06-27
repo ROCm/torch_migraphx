39ace76475ab777cb7fb1e329bddb590e506d46b
Shiv <Shivad.Bhavsar@amd.com>
2023-06-27 13:46:42 -0700

    only import dynamo path for new torch versions

--- py/torch_migraphx/__init__.py
+++ py/torch_migraphx/__init__.py
@@ -1,1 +1,6 @@
-from torch_migraphx import fx, _C, dynamo
+from packaging import version
+import torch
+from torch_migraphx import fx, _C
+
+if version.parse(torch.__version__) >= version.parse("2.1.dev"):
+    from torch_migraphx import dynamo
