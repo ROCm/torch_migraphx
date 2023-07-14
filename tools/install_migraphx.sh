1e09e73e15766fb921ca5a3fa9bb8c20ecc872bb
Shiv <Shivad.Bhavsar@amd.com>
2023-07-14 20:10:53 +0000

    add explicit install path

--- tools/install_migraphx.sh
+++ tools/install_migraphx.sh
@@ -4,7 +4,7 @@ MIGRAPHX_REPO=https://github.com/ROCmSoftwarePlatform/AMDMIGraphX.git
 git clone --single-branch --branch $MIGRAPHX_BRANCH --recursive $MIGRAPHX_REPO
 cd AMDMIGraphX
 
-rbuild build -d depend -DBUILD_DEV=On --cxx=/opt/rocm/llvm/bin/clang++ 
+rbuild build -d depend -DCMAKE_INSTALL_PREFIX=/opt/rocm/ -DBUILD_DEV=On --cxx=/opt/rocm/llvm/bin/clang++ 
 
 cd build
 make install
