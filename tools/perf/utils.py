19708daa29af78702d1e017e3bbc7aeccb1dbcc0
Shivad Bhavsar <shbhavsa@amd.com>
2023-12-20 01:02:29 +0000

    initial perf script updates

--- tools/perf/utils.py
+++ tools/perf/utils.py
@@ -1,10 +1,14 @@
-from typing import Sequence
+from typing import Sequence, Mapping, Any
 import torch
+from tabulate import tabulate
 
 
-def benchmark_module(iterations: int, model: torch.nn.Module,
-                     inputs: Sequence[torch.Tensor]) -> float:
-    model(*inputs)
+def benchmark_module(model: torch.nn.Module,
+                     inputs: Sequence[torch.Tensor],
+                     iterations: int,
+                     kwargs: Mapping[str, Any] = None) -> float:
+    kwargs = {} if kwargs is None else kwargs
+    model(*inputs, **kwargs)
     torch.cuda.synchronize()
 
     start_event = torch.cuda.Event(enable_timing=True)
@@ -12,8 +16,22 @@ def benchmark_module(iterations: int, model: torch.nn.Module,
 
     start_event.record()
     for _ in range(iterations):
-        model(*inputs)
+        model(*inputs, **kwargs)
     end_event.record()
     torch.cuda.synchronize()
 
     return start_event.elapsed_time(end_event) / iterations
+
+
+def print_bm_results(names: Sequence[str],
+                     times: Sequence[float],
+                     bs: int,
+                     ref_idx: int = 0):
+    t_ref = times[ref_idx]
+    rows = []
+    headers = ["Model", "Avg Exec Time (ms)", "Rate (/sec)", "Speed Up"]
+    for n, t in zip(names, times):
+        rows.append(
+            [n, f"{t:0.4f}", f"{1e3 * bs / t:0.4f}", f"{t_ref/t:0.4f}x"])
+
+    print(tabulate(rows, headers=headers))
