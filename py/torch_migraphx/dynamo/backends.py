2d3dda4137298aa9a813b3a723e40dcfcd3d575c
shivadbhavsar <105248561+shivadbhavsar@users.noreply.github.com>
2024-02-14 09:13:29 -0800

    Dynamo save (#70)
    * add flag to save compiled model in torch.compile workflow
    
    * update install script to include mi300 series targets

--- py/torch_migraphx/dynamo/backends.py
+++ py/torch_migraphx/dynamo/backends.py
@@ -38,7 +38,7 @@ from .lower_dynamo import lower_aten_to_mgx
 @dynamo.register_backend(name="migraphx")
 def migraphx_backend(gm: torch.fx.GraphModule,
                      example_inputs: Sequence[torch.Tensor], **kwargs):
-    
+
     # Any logic to pick default dynamo backend should be placed here
     return migraphx_aot_backend(gm, example_inputs, **kwargs)
 
@@ -49,13 +49,16 @@ def migraphx_aot_backend(gm: torch.fx.GraphModule,
 
     # Any addition kwargs are captrued through the "options" key
     kwargs = kwargs["options"] if "options" in kwargs else kwargs
-    
-    # TODO: cleaner way to invoke aot autograd with real inputs?
+
+    if "load_compiled" in kwargs:
+        return torch.load(kwargs["load_compiled"])
+
     TracingContext.get().fake_mode.allow_non_fake_inputs = True
 
-    # TODO: Investigate other variants for invoking aot_autograd:
-    # https://github.com/pytorch/pytorch/blob/main/torch/_functorch/aot_autograd.py
-    
     aten_gm = aot_export_joint_simple(gm, example_inputs, trace_joint=False)
 
-    return lower_aten_to_mgx(aten_gm, example_inputs, **kwargs)
+    compiled_gm = lower_aten_to_mgx(aten_gm, example_inputs, **kwargs)
+    if "save_compiled" in kwargs:
+        torch.save(compiled_gm, kwargs["save_compiled"], pickle_protocol=4)
+
+    return compiled_gm
--- tools/install_migraphx.sh
+++ tools/install_migraphx.sh
@@ -1,6 +1,6 @@
 MIGRAPHX_BRANCH=${1:-"master"}
 MIGRAPHX_REPO=https://github.com/ROCm/AMDMIGraphX.git
-GPU_ARCH=${2:-"gfx900;gfx906;gfx908;gfx90a;gfx1030;gfx1100;gfx1101;gfx1102;gfx940"}
+GPU_ARCH=${2:-"gfx900;gfx906;gfx908;gfx90a;gfx1030;gfx1100;gfx1101;gfx1102;gfx940;gfx941;gfx942"}
 
 # Install rbuild
 pip3 install https://github.com/RadeonOpenCompute/rbuild/archive/master.tar.gz
@@ -18,7 +18,7 @@ cd ../..
 git clone --single-branch --branch $MIGRAPHX_BRANCH --recursive $MIGRAPHX_REPO
 cd AMDMIGraphX
 
-rbuild build -d depend -DBUILD_TESTING=Off -DCMAKE_INSTALL_PREFIX=/opt/rocm/ -DBUILD_DEV=On --cxx=/opt/rocm/llvm/bin/clang++ -DGPU_TARGETS=$GPU_ARCH
+rbuild build -d depend -DBUILD_TESTING=Off -DCMAKE_INSTALL_PREFIX=/opt/rocm/ --cxx=/opt/rocm/llvm/bin/clang++ -DGPU_TARGETS=$GPU_ARCH
 
 cd build
 make install
