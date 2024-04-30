5b751c1a754ba4b12d58b66730db2fd18f849042
Shiv <Shivad.Bhavsar@amd.com>
2024-04-29 17:18:48 -0700

    remove unnecessary import

--- py/torch_migraphx/dynamo/backends.py
+++ py/torch_migraphx/dynamo/backends.py
@@ -27,7 +27,6 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #####################################################################################
 from typing import Sequence
-import unittest
 
 import torch
 import torch._dynamo as dynamo
