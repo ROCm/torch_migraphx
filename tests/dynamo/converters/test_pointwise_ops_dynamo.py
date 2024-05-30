228df110c27c574ca3d45910409781789ae00887
Brian Pickrell <95253842+bpickrel@users.noreply.github.com>
2024-05-30 12:01:51 -0700

    add converter for sqrt function (#127)
    Co-authored-by: shivadbhavsar <105248561+shivadbhavsar@users.noreply.github.com>

--- tests/dynamo/converters/test_pointwise_ops_dynamo.py
+++ tests/dynamo/converters/test_pointwise_ops_dynamo.py
@@ -13,6 +13,7 @@ if not hasattr(torch_migraphx, "dynamo"):
     torch.ops.aten.exp.default,
     torch.ops.aten.neg.default,
     torch.ops.aten.sin.default,
+    torch.ops.aten.sqrt.default,
 ])
 def test_unary_func(op_alias):
     inp = torch.randn(2, 9, 11, 1).cuda()
