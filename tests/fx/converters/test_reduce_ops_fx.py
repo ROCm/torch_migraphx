75cbd7374c92f28d81a87bbc52b105fda79b88dc
Charlie Lin <charlie.lin@amd.com>
2024-07-18 12:30:52 -0400

    Update tests/fx/converters/test_reduce_ops_fx.py
    Co-authored-by: shivadbhavsar <105248561+shivadbhavsar@users.noreply.github.com>

--- tests/fx/converters/test_reduce_ops_fx.py
+++ tests/fx/converters/test_reduce_ops_fx.py
@@ -102,6 +102,7 @@ def test_cumsum(dim):
         verify_outputs(mod, mgx_mod, inp)
 
 
+@pytest.mark.skip_min_migraphx_ver("2.11.0")
 @pytest.mark.parametrize('dim, keepdim', [(0, True), (-1, False),
                                           ([2, 3], False), (None, None)])
 def test_any(dim, keepdim):
