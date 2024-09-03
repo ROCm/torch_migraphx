9956691d96aaf7f7f8e50cbd67a95ccdce47acdd
shivadbhavsar <105248561+shivadbhavsar@users.noreply.github.com>
2024-09-03 10:18:18 -0700

    fix master merge

--- tests/dynamo/converters/test_pointwise_ops_dynamo.py
+++ tests/dynamo/converters/test_pointwise_ops_dynamo.py
@@ -227,4 +227,4 @@ def test_erf(op_alias):
     inp = torch.randn(2, 9, 11, 1).cuda()
     mod = FuncModule(op_alias).cuda()
     mgx_mod = convert_to_mgx(mod, [inp])
-
+    verify_outputs(mod, mgx_mod, inp)
