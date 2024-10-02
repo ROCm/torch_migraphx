cd9b8a8a10bb0438f6145102552773d05e915ae4
Shiv <Shivad.Bhavsar@amd.com>
2024-10-02 15:51:05 -0700

    change import order as temp fix for issues with package manager installed deps

--- py/torch_migraphx/__init__.py
+++ py/torch_migraphx/__init__.py
@@ -1,6 +1,6 @@
 from packaging import version
-import migraphx
 from torch import __version__ as _torch_version
+import migraphx
 from torch_migraphx import fx, _C
 
 if version.parse(_torch_version) >= version.parse("2.1.0"):
