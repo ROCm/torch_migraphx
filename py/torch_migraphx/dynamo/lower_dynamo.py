f4a9928e2818c8569b4c1cef3b9a2ac20514a804
Shiv <Shivad.Bhavsar@amd.com>
2025-01-09 18:07:11 -0800

    allow passing prefix to save_mxr compile option

--- py/torch_migraphx/dynamo/lower_dynamo.py
+++ py/torch_migraphx/dynamo/lower_dynamo.py
@@ -113,7 +113,8 @@ def lower_subgraph(module: torch.fx.GraphModule,
     interpreter.run()
 
     if save_mxr:
-        name = f"{kwargs['name']}.mxr" if 'name' in kwargs else "prog.mxr"
+        prefix = save_mxr if isinstance(save_mxr, str) else ""
+        name = f"{prefix}_{kwargs['name']}.mxr" if 'name' in kwargs else f"{prefix}_prog.mxr"
         migraphx.save(interpreter.program, name)
 
     if print_uncompiled: interpreter.program.print()
