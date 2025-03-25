da16a61b45b98c5db3419901e35c77f1998566c4
richagadgil <richa.gadgil@amd.com>
2025-03-25 14:44:51 -0500

    finish passes

--- py/torch_migraphx/dynamo/passes/pass_manager.py
+++ py/torch_migraphx/dynamo/passes/pass_manager.py
@@ -29,7 +29,7 @@
 import torch
 from torch.fx.passes.pass_manager import PassManager
 
-from .remove_ops import remove_const_ops, remove_view_ops, remove_mul_complex_ops
+from .remove_ops import remove_const_ops, remove_view_ops, remove_mul_complex_ops, remove_complex_real_ops
 from .const_fold import const_fold
 from .promote_types import promote_inputs
 from .remove_empty_slice import remove_empty_slices
@@ -49,8 +49,9 @@ def pre_partition_pass(gm: torch.fx.GraphModule) -> torch.fx.GraphModule:
         remove_view_ops,
         promote_inputs,
         remove_empty_slices,
-        const_fold,
         remove_mul_complex_ops,
+        remove_complex_real_ops,
+        const_fold,
     ]
     pre_partition_pass_mgr = MGXPassManager(passes)
     return pre_partition_pass_mgr(gm)
--- test.py
+++ test.py
@@ -5,7 +5,8 @@ import torch_migraphx
 # call_function  mul_tensor_7                                        aten.mul.Tensor                                       (view_as_complex_default, reshape_3)                                                                         {}
 # call_function  view_as_real_default                                aten.view_as_real.default                             (mul_tensor_7,)    
 torch.manual_seed(4)
-complex_const = torch.randn(2,2, dtype=torch.cfloat)
+complex_const = torch.randn(3, 2, 2, dtype=torch.cfloat)
+x = torch.randn(3, 2, 2, 2)
 
 class ComplexMul(torch.nn.Module):
     def __init__(self, *args, **kwargs):
@@ -18,8 +19,7 @@ class ComplexMul(torch.nn.Module):
         return torch.view_as_real(x_complex * self.const)
 
 mod = ComplexMul()
-x = torch.randn(2,2,2)
-out = mod(x)
+out1 = mod(x)
 
 class RealMul(torch.nn.Module):
     def __init__(self, *args, **kwargs):
@@ -33,18 +33,43 @@ class RealMul(torch.nn.Module):
         c_real, c_imag = c[..., 0], c[..., 1]
         out_real = x_real * c_real - x_imag * c_imag
         out_imag = x_real * c_imag + x_imag * c_real
-        print(c_real) 
         return torch.stack((out_real, out_imag), dim=-1)
+
+    import torch
 
 mod2 = RealMul()
 out2 = mod2(x)
+
+def real_mul_aten_ops(x, complex_const):
+    c = torch.ops.aten.view_as_real.default(complex_const)
+    
+    x_real = torch.ops.aten.select.int(x, dim=-1, index=0)
+    x_imag = torch.ops.aten.select.int(x, dim=-1, index=1)
+
+    c_real = torch.ops.aten.select(c, dim=-1, index=0)
+    c_imag = torch.ops.aten.select(c, dim=-1, index=1)
+
+    out_real = torch.ops.aten.sub.Tensor(
+        torch.ops.aten.mul.Tensor(x_real, c_real),
+        torch.ops.aten.mul.Tensor(x_imag, c_imag)
+    )
+    out_imag = torch.ops.aten.add.Tensor(
+        torch.ops.aten.mul.Tensor(x_real, c_imag),
+        torch.ops.aten.mul.Tensor(x_imag, c_real)
+    )
+
+    out = torch.ops.aten.stack.default([out_real, out_imag], dim=-1)
+
+    return out
+
+out3 = real_mul_aten_ops(x, complex_const)
 
 with torch.inference_mode():
     mod_mgx = torch.compile(mod, backend="migraphx", options={"verbose": True, "print_compiled_program": True})
     mgx_out = mod_mgx(x)
 
-assert torch.allclose(out, out2) 
-assert torch.allclose(out, mgx_out)
 
-print("\n\n")
-print(mgx_out)
+assert torch.allclose(out1, mgx_out.cpu().detach())
+assert torch.allclose(out2, mgx_out.cpu().detach())
+assert torch.allclose(out3, mgx_out.cpu().detach())
+
