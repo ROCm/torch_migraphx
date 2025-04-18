cca0c5515550b2fc561c8350e39cefc4e8bda405
Shiv <shbhavsa@amd.com>
2025-04-18 12:56:42 -0500

    update to work with older versions

--- py/torch_migraphx/csrc/torch_migraphx_py.cpp
+++ py/torch_migraphx/csrc/torch_migraphx_py.cpp
@@ -32,13 +32,17 @@
 
 torch::Tensor from_blob_wrapper(std::uintptr_t ptr, const std::vector<int64_t> &sizes,
                                 const std::vector<std::int64_t> &strides,
-                                torch::ScalarType dtype, torch::Device device) {
+                                py::object dtype, torch::Device device)
+{
 
-  auto options = torch::TensorOptions().dtype(dtype).device(device);
+  // Old torchlib versions cannot take torch::ScalarType directly as a function argument
+  torch::ScalarType scalar_type = reinterpret_cast<THPDtype *>(dtype.ptr())->scalar_type;
+  auto options = torch::TensorOptions().dtype(scalar_type).device(device);
   return torch::from_blob(reinterpret_cast<void *>(ptr), sizes, strides, options);
 }
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
   m.def("from_blob", &from_blob_wrapper,
         "Create a tensor from a blob of data.");
 }
