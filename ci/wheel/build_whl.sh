3f0eefbcadc3fcc41cad6f949c42803da509d10c
Shiv <Shivad.Bhavsar@amd.com>
2025-08-28 22:15:46 +0000

    remove torch install

--- ci/wheel/build_whl.sh
+++ ci/wheel/build_whl.sh
@@ -6,13 +6,10 @@ export PROJECT_DIR=/workspace/torch_migraphx
 
 # PY_BUILD_CODE, PY_VERSION
 build_audit_whl() {
-    /opt/python/$1/bin/python -m pip install torch==2.6.0 torchvision==0.21.0 -f https://repo.radeon.com/rocm/manylinux/rocm-rel-6.4/
-    TORCH_LIB_DIR=/opt/python/$1/lib/python$2/site-packages/torch/lib/
-
     /opt/python/$1/bin/python setup.py clean bdist_wheel
 
     /opt/python/$1/bin/python -m pip install auditwheel
-    LD_LIBRARY_PATH=${TORCH_LIB_DIR}:${LD_LIBRARY_PATH} /opt/python/$1/bin/python -m auditwheel repair --plat manylinux_2_28_x86_64 dist/torch_migraphx-*-$1-linux_x86_64.whl
+    LD_LIBRARY_PATH=${LD_LIBRARY_PATH} /opt/python/$1/bin/python -m auditwheel repair --plat manylinux_2_28_x86_64 dist/torch_migraphx-*-$1-linux_x86_64.whl
 }
 
 build_py39(){
